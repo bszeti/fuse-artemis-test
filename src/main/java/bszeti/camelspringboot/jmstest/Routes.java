0fd87d37fb834cb276487f61a278aca668f984cb
Szeti, balazs <bszeti@mtahq.org>
2020-10-17 13:23:02 +0200

    add counter

--- src/main/java/bszeti/camelspringboot/jmstest/Routes.java
+++ src/main/java/bszeti/camelspringboot/jmstest/Routes.java
@@ -16,6 +16,7 @@ public class Routes extends RouteBuilder {
 
     private AtomicInteger receiveCounter = new AtomicInteger();
     private int receiveCounterLast = 0;
+    private AtomicInteger receiveForwardedCounter = new AtomicInteger();
     private AtomicInteger sendCounter = new AtomicInteger();
     private int sendCounterLast = 0;
 
@@ -36,7 +37,7 @@ public class Routes extends RouteBuilder {
 
         onException(Exception.class)
             .maximumRedeliveries(0)
-            .log(LoggingLevel.ERROR,"onException: ${exception}")
+            .log(LoggingLevel.ERROR,"Camel onException: ${exception}")
         ;
 
         from("amqp:{{receive.endpoint}}")
@@ -48,50 +49,48 @@ public class Routes extends RouteBuilder {
                 .throwException(new Exception("error"))
             .end()
 
+            .setHeader("receiveCounter").exchange(e->receiveCounter.incrementAndGet())
+
             .choice()
                 .when(constant("{{receive.forward.enabled}}"))
                 .delay(constant("{{receive.forward.delay}}"))
-                .log(LoggingLevel.DEBUG, log, "Message forward: ${exchangeId}")
+                .log(LoggingLevel.DEBUG, log, "Message forward: ${exchangeId} - ${header.counter}")
                 .to("amqp:{{receive.forward.endpoint}}")
+                .process(e-> receiveForwardedCounter.incrementAndGet())
             .end()
             
             .delay(constant("{{receive.delay}}"))
             .log(LoggingLevel.DEBUG, log, "Message processed: ${exchangeId}")
-            .process(e-> receiveCounter.incrementAndGet())
         ;
 
+        from("timer:sender?period=1&repeatCount={{send.threads}}")
+            .routeId("amqp.send").autoStartup("{{send.enabled}}")
 
-            from("timer:sender?period=1&repeatCount={{send.threads}}")
-                .routeId("amqp.send").autoStartup("{{send.enabled}}")
- 
-                    .threads().poolSize(sendThreads).maxPoolSize(sendThreads).maxQueueSize(sendThreads).rejectedPolicy(ThreadPoolRejectedPolicy.CallerRuns)
-                        
-                        .log(LoggingLevel.INFO, log, "Sending {{send.count}}")
-                        .loop(constant("{{send.count}}"))
-                            .log(LoggingLevel.DEBUG, log, "Send msg: ${exchangeId}-${header.CamelLoopIndex}")
-                            
-                            .setBody(simple(sendMessage))
-                            .setHeader("{{send.headeruuid}}").exchange(e->java.util.UUID.randomUUID().toString())
-                            .to("amqp:{{send.endpoint}}?transacted=false")
-                            .process(e-> sendCounter.incrementAndGet())
-                            .delay(constant("{{send.delay}}"))
-                            .log(LoggingLevel.DEBUG, log, "Sent msg: ${exchangeId}-${header.CamelLoopIndex} - ${body}")
-                        .end()
+                .threads().poolSize(sendThreads).maxPoolSize(sendThreads).maxQueueSize(sendThreads).rejectedPolicy(ThreadPoolRejectedPolicy.CallerRuns)
+
+                    .log(LoggingLevel.INFO, log, "Sending {{send.count}}")
+                    .loop(constant("{{send.count}}"))
+                        .log(LoggingLevel.DEBUG, log, "Send msg: ${exchangeId}-${header.CamelLoopIndex}")
+
+                        .setBody(simple(sendMessage))
+                        .setHeader("{{send.headeruuid}}").exchange(e->java.util.UUID.randomUUID().toString())
+                        .to("amqp:{{send.endpoint}}?transacted=false")
+                        .process(e-> sendCounter.incrementAndGet())
+                        .delay(constant("{{send.delay}}"))
+                        .log(LoggingLevel.DEBUG, log, "Sent msg: ${exchangeId}-${header.CamelLoopIndex} - ${body}")
                     .end()
-                .log(LoggingLevel.INFO, log, "Done - {{send.count}}")
+                .end()
+            .log(LoggingLevel.INFO, log, "Done - {{send.count}}")
         ;
-
-
-
-
 
         from("timer:printCounter?period=1000")
             .setBody(b->{
                 if (receiveEnabled) {
+                    int forwarder = receiveForwardedCounter.get();
                     int current = receiveCounter.get();
                     int diff = current - receiveCounterLast;
                     receiveCounterLast = current;
-                    return "receive: " + current + " - " + diff + "/s";
+                    return "receive: " + current + " (" + forwarder + ") "+ " - " + diff + "/s";
                 }
                 if (sendEnabled) {
                     int current = sendCounter.get();
